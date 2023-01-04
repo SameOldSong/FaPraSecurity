9814827e519a640077718ddfef73dd085c380e59
SameOldSong <43583853+SameOldSong@users.noreply.github.com>
2023-01-04 18:00:49 +0100

    Add files via upload

---
+++ scripts/1_3_db_seafile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+echo "CREATING DB USER for seafile"
+kathara exec database -- mariadb -e "CREATE USER seafile@192.168.1.11 IDENTIFIED VIA ed25519 USING PASSWORD('N0=22Sy-Fa?42');"
+
+echo "CREATING DATABASES for seafile"
+kathara exec database -- mariadb -e "create database ccnet_db character set = utf8;"
+kathara exec database -- mariadb -e "create database seafile_db character set = utf8;"
+kathara exec database -- mariadb -e "create database seahub_db character set = utf8;"
+
+echo "PERMISSIONS for seafile user"
+
+kathara exec database -- mariadb -e "GRANT ALL PRIVILEGES ON ccnet_db.* to 'seafile'@'192.168.1.11';"
+kathara exec database -- mariadb -e "GRANT ALL PRIVILEGES ON seafile_db.* to 'seafile'@'192.168.1.11';"
+kathara exec database -- mariadb -e "GRANT ALL PRIVILEGES ON seahub_db.* to 'seafile'@'192.168.1.11';"
+
+
+
+
+
+
+
+ 
+ 
