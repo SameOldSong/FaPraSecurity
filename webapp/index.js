fdf75515a2756ca0a18f7a2906fb9a5b92277ccd
SameOldSong <43583853+SameOldSong@users.noreply.github.com>
2023-01-14 19:41:01 +0100

    Update index.js

--- webapp/index.js
+++ webapp/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const mariadb = require('mariadb');
-
+const fs = require('fs');
 const app = express();
 const port = 5000;
+const serverCert = [fs.readFileSync("/var/www/fapraweb/server-cert.pem"), "utf8"];
+app.use(express.static('/var/www/fapraweb/webapp/public'));
 
-app.use(express.static('public'));
+const clientKey = [fs.readFileSync("/var/www/fapraweb/client-key.pem")];
+const clientCert = [fs.readFileSync("/var/www/fapraweb/client-cert.pem")];
 
 //Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+app.get('/', (req, res) => {        
+    res.sendFile('index.html', {root: __dirname});     
 });
 
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+app.listen(port, () => {            
     console.log(`Now listening on port ${port}`); 
 });
 
@@ -22,33 +24,33 @@ app.get('/clicked', (req, res) => {
   console.log(click);
   var params = [click];
  
-  const pool = mariadb.createPool({
-     host: process.env.DB_HOST,
-     user: process.env.DB_USER,
-     password: process.env.DB_PWD,
-     connectionLimit: 5
-});
+mariadb
+ .createConnection({
+   host: 'database', 
+   ssl: {
+	//only for TESTING, not for PRODUCTION!!!
+	rejectUnauthorized: false,
+	
+	ca: serverCert,
+	cert: clientCert,
+	key: clientKey
+   }, 
+   user: 'fapraweb'
+ }).then(conn => {
 
-  pool.getConnection()
-    .then(conn => {
-      var sql = "SELECT quote FROM fapraweb.quotes WHERE item_id = ?";  
-      conn.query(sql, params)
-        .then((result) => {
-          console.log(result);
- res.send(result);
-          conn.end();
-        })
-        .catch(err => {
-          //handle error
-          console.log(err);
-          conn.end();
-        })
-       
-    }).catch(err => {
-      //not connected
-    });
-
-
+	 console.log("Works");
   
+	 var sql = "SELECT quote FROM fapraweb.quotes WHERE item_id = ?";
+	 conn.query(sql, params).then((result) => {
+	   console.log(result);
+	   res.send(result); conn.end();
+
+	 }).catch(error => { 
+		 console.log("something wrong with query"); 
+		 console.log(error);
+	 });
+ }).catch(error => { console.log(error); console.log("something wrong with connection");});
+
+
 });
 
