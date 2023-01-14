711031d65b28406e14b9e7fe293670965e7c78c5
SameOldSong <43583853+SameOldSong@users.noreply.github.com>
2023-01-14 23:22:15 +0100

    Update 1_2_db_webapp.sh

--- scripts/1_2_db_webapp.sh
+++ scripts/1_2_db_webapp.sh
@@ -28,27 +28,3 @@ kathara exec database -- sed -i 's/bind-address/#bind-address/g' /etc/mysql/mari
 
 kathara exec database -- service mariadb restart
 
-echo "ADJUSTING  IPTABLES"
-kathara exec database -- iptables -A INPUT -p tcp --dport 3306 -s 192.168.1.12 -j ACCEPT
- 
-kathara exec database -- iptables -A INPUT -p tcp --dport 3306 -s 192.168.1.11 -j ACCEPT
-
-kathara exec database -- iptables -A OUTPUT -p tcp --sport 3306 -d 192.168.1.11 -m state --state ESTABLISHED -j ACCEPT
-
-kathara exec database -- iptables -A OUTPUT -p tcp --sport 3306 -d 192.168.1.12 -m state --state ESTABLISHED -j ACCEPT
-
-kathara exec database -- iptables -P OUTPUT DROP
-kathara exec database -- iptables -P INPUT DROP
-kathara exec database -- iptables -P FORWARD DROP
-
-
-echo "PERSISTING  IPTABLES"
-kathara exec database -- /bin/sh -c "iptables-save > /etc/iptables/rules.v4"
-
-
-
-
-
-
- 
- 
