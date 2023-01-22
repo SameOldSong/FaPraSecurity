20ca595c03cf6fd7e5f4e187497c961792fbd522
SameOldSong <tatiana.nickolaeva@gmail.com>
2023-01-22 18:42:06 +0100

    Web server certificates

--- scripts/all.sh
+++ scripts/all.sh
@@ -7,3 +7,4 @@ sh  /home/lab/labs/Unternehmensnetzwerk/FaPraSecurity/scripts/0_1_routerdmz_masq
 sh  /home/lab/labs/Unternehmensnetzwerk/FaPraSecurity/scripts/1_1_db_preparation.sh
 sh  /home/lab/labs/Unternehmensnetzwerk/FaPraSecurity/scripts/1_2_db_webapp.sh
 sh  /home/lab/labs/Unternehmensnetzwerk/FaPraSecurity/scripts/1_3_db_seafile.sh
+sh  /home/lab/labs/Unternehmensnetzwerk/FaPraSecurity/scripts/2_1_webserver.sh
